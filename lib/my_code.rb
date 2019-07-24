f203f183081dc604db81aff36cf7776e4acd8465
Sarah Drake <jussaradrake@outlook.com>
2019-07-24 13:12:11 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -30,10 +30,10 @@ def reduce_to_all_true(source_array)
 
 
 def reduce_to_any_true(source_array)
-  i = 0	
-   while i < source_array.length do	
-     return true if source_array[i]	
-     i += 1	
-   end	
-   return false	
+  i = 0
+   while i < source_array.length do
+     return true if source_array[i]
+     i += 1
+   end
+   return false
  end
